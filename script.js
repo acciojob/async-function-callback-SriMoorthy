c432511754699124a3e99ea9a9eb9e701733d468
SriMoorthy <srimoorthy27@gmail.com>
2024-10-17 23:20:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 const url = "https://jsonplaceholder.typicode.com/posts/1";
+let out = document.getElementById('output');
+
 
 //your JS code here. If required.
+async function callback(){
+	let res = await fetch(url);
+	let data = await res.json()
+	return new Promise((resolve)=>{
+		resolve(out.innerHTML = data.title)
+	})
+}
